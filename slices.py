adf3471e73db2691494d0d7d451a3f7f388584ee
Yasmin <yas.zkm@gmail.com>
2020-06-11 21:19:15 +0300

    Random snippets - improved.

--- slices.py
+++ slices.py
@@ -1,16 +1,9 @@
-import sys
+import numpy as np
 from os import listdir
 from os.path import isfile, join
 import os
 import random
-from itertools import islice
 from enum import Enum, unique
-
-input_path = "github_data/"
-output_path = "github_data_slices/"
-
-MIN_LINES_PER_SLICE = 1
-MAX_LINES_PER_SLICE = 5
 
 
 @unique
@@ -23,33 +16,41 @@ def print_msg(msg_type, msg):
     print("[" + msg_type.value + "] " + str(msg))
 
 
-def main():
-    if not os.path.exists(input_path):
-        print_msg(MsgTypes.ERROR, "Cannot find the path specified: " + input_path)
-        exit(1)
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-    original = sys.stdout
-
-    input_files = [f for f in listdir(input_path) if isfile(join(input_path, f))]
-    print_msg(MsgTypes.INFO, "Input :" + str(input_files))
-
-    for file in input_files:
-        print_msg(MsgTypes.INFO, ">> " + file)
-        with open(input_path + file, mode="r", encoding="utf-8") as input_file:
-            output_file = "_" + file.replace("all_data", "all_data_", 1)
-            i = 0
-            with open(output_path + output_file, mode="w", encoding="utf-8") as file_handle:
-                while True:
-                    sys.stdout = file_handle
-                    random_num_lines = random.randint(MIN_LINES_PER_SLICE, MAX_LINES_PER_SLICE)
-                    lines = list(islice(input_file, random_num_lines))
-                    if not lines:
-                        sys.stdout = original
-                        break
-                    print(lines)
-                    sys.stdout = original
+def check_dirs(input_dir, output_dir):
+    if not os.path.exists(input_dir):
+        print_msg(MsgTypes.ERROR, "Cannot find the path specified: " + input_dir)
+        exit(0)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
 
-main()
+def create_snippets(total_samples, input_dir, output_dir, min_lines_per_slice=1, max_lines_per_slice=5):
+    input_str = []
+    input_lbl = []
+    check_dirs(input_dir, output_dir)
+
+    input_files = [f for f in listdir(input_dir) if isfile(join(input_dir, f))]
+    total_num_of_files = len(input_files)
+    print_msg(MsgTypes.INFO, "Input Files:" + str(input_files))
+
+    while total_samples:
+        random_file_index = random.randint(0, total_num_of_files-1)
+        print_msg(MsgTypes.INFO, ">> " + input_files[random_file_index])
+
+        random_num_code_lines = random.randint(min_lines_per_slice, max_lines_per_slice)
+
+        with open(input_dir + input_files[random_file_index], mode="r", encoding="utf-8") as input_file:
+            all_file_lines = input_file.readlines()
+            file_len = len(all_file_lines)
+            random_starting_line = random.randint(1, file_len) - 1
+            if random_starting_line + random_num_code_lines > file_len:
+                lines = all_file_lines[random_starting_line:]
+            else:
+                lines = all_file_lines[random_starting_line:random_starting_line+random_num_code_lines]
+            input_str.append("".join(lines))
+            input_lbl.append(random_file_index)
+        total_samples = total_samples - 1
+    return input_str, input_lbl
+
+
+create_snippets(20, "github_data/", "github_data_slices/")
